d5a4b24d45e1dcc3dbe041bd0d869a1ee1940372
Garrett Hunter <garrett.hunter@blacktower.com>
2017-03-23 11:21:05 -0700

    bug fixes

--- GoogleCloud/debian/initialize.sh
+++ GoogleCloud/debian/initialize.sh
@@ -116,7 +116,7 @@ function getWordPRess {
 
         #
         # Clean up
-        rm /var/www/html/index.html /var/www/html/readme.html /var/www/html/license.txt
+        # rm /var/www/html/index.html /var/www/html/readme.html /var/www/html/license.txt
         rm -rf wordpress latest.tar.gz
     else
         echo "Missing ${TEMPDIR} directory."
